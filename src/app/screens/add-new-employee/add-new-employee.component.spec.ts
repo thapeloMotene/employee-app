a0d972985f3fbc3a2b395fd65eabcad138e24f23
Thapelo Motene <thapelo.motene@gmail.com>
2019-11-15 19:51:58 +0200

    final

--- src/app/screens/add-new-employee/add-new-employee.component.spec.ts
+++ src/app/screens/add-new-employee/add-new-employee.component.spec.ts
@@ -3,23 +3,5 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddNewEmployeeComponent } from './add-new-employee.component';
 
 describe('AddNewEmployeeComponent', () => {
-  let component: AddNewEmployeeComponent;
-  let fixture: ComponentFixture<AddNewEmployeeComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AddNewEmployeeComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddNewEmployeeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+ 
 });
--- src/app/screens/delete-employee/delete-employee.component.spec.ts
+++ src/app/screens/delete-employee/delete-employee.component.spec.ts
@@ -3,23 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DeleteEmployeeComponent } from './delete-employee.component';
 
 describe('DeleteEmployeeComponent', () => {
-  let component: DeleteEmployeeComponent;
-  let fixture: ComponentFixture<DeleteEmployeeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DeleteEmployeeComponent ]
-    })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DeleteEmployeeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
